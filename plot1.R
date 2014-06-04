814eea9479ae259bae4a603060c7825f9e962af7
Alicia W <rainingilove@gmail.com>
2014-06-04 16:45:19 +0200

    Reading specific date only.

---
+++ plot1.R
@@ -0,0 +1,36 @@
+plot1 <- function(directory) {
+    filename <- "household_power_consumption.txt"
+    fullname <- paste(directory, "/", filename, sep = "")
+    
+    DF.row1 <- read.csv(fullname, header = TRUE, sep = ";", nrows = 1)
+    nc <- ncol(DF.row1)
+    
+    DF.Date <- read.csv(fullname, header = TRUE, sep = ";", na.strings = "?", 
+                          colClasses = c("character", rep("NULL", nc - 1)))
+    
+    # Convert the date format in the file to R format
+    DF.Date.f <- strptime(DF.Date$Date, "%d/%m/%Y")
+    
+    # Set the start and end date
+    startdate <- as.Date("2007-01-31")
+    startdate <- as.POSIXct(startdate)
+    enddate <- as.Date("2007-02-02")
+    enddate <- as.POSIXct(enddate)
+    
+    print(startdate)
+    print(enddate)
+
+    nskip <- which.max(DF.Date.f > startdate)
+    nend <- which.max(DF.Date.f > enddate)
+    
+    nreadrow <- nend - nskip
+    
+    print(nskip)
+    print(nend)
+    print(nreadrow)
+    
+    effdata <- read.csv(fullname, col.names = names(DF.row1), colClasses = "character", skip = nskip, 
+                        nrow = nreadrow, sep = ";", na.strings = "?")
+    print(head(effdata))
+    print(tail(effdata))
+}
