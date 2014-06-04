80e911ea9dd34dd99ba56d76d28321cf0dfca7c9
Alicia W <rainingilove@gmail.com>
2014-06-04 16:50:20 +0200

    Some comments are added to the datafile reading program.

--- plot1.R
+++ plot1.R
@@ -1,10 +1,20 @@
+# Author : Alicia Wang
+# Date : 4 June 2014
+# 
+#
+#--------------------------------------
+
 plot1 <- function(directory) {
+    
+    # Assemble the full name of the data file with path
     filename <- "household_power_consumption.txt"
     fullname <- paste(directory, "/", filename, sep = "")
     
+    # Read the first row of the data file
     DF.row1 <- read.csv(fullname, header = TRUE, sep = ";", nrows = 1)
     nc <- ncol(DF.row1)
     
+    # Read the first column to get the date of each row
     DF.Date <- read.csv(fullname, header = TRUE, sep = ";", na.strings = "?", 
                           colClasses = c("character", rep("NULL", nc - 1)))
     
@@ -29,6 +39,7 @@ plot1 <- function(directory) {
     print(nend)
     print(nreadrow)
     
+    # Only read between the start and end date of the file
     effdata <- read.csv(fullname, col.names = names(DF.row1), colClasses = "character", skip = nskip, 
                         nrow = nreadrow, sep = ";", na.strings = "?")
     print(head(effdata))
